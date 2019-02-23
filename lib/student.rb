b9feff453a074f5fcade31f0ca33d1de859730bc
Brian Di Rito <bdirito91@gmail.com>
2019-02-23 16:59:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,14 @@
 class Student < User
   attr_accessor :first_name, :last_name
   
+  @@all = []
+  
   def initialize
     @knowledge = []
+    @@all << self
+  end
+  
+  def learn(str)
+    @knowledge << str
   end
 end
