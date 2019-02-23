b2955c6ba25a52f9fac484e2801999d49345ffda
Brian Di Rito <bdirito91@gmail.com>
2019-02-23 17:01:39 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,7 +1,7 @@
 class Teacher < User
   attr_accessor :first_name, :last_name
   
-  KNOWLEDGE = 
+  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async we...instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
   
   def teach
     
